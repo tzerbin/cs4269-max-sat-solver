6bb747b550e762a8b1ed93d8b2d4777cb2d7d5d8
purplepers0n <choo.wx96@gmail.com>
2019-10-07 15:24:23 +0800

    Implement cnf generator

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+import generator.CnfGenerator;
 import parser.CnfParser;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
+        CnfGenerator cnfGenerator = CnfGenerator.getInstance();
         CnfParser cnfParser = CnfParser.getInstance();
 
-        cnfParser.parseCnf("");
+        String CNF = cnfGenerator.generateCNF(5, 1, 4, 8);
+        System.out.println("CNF: " + CNF);
+        cnfParser.parseCnf(CNF);
     }
 }
--- src/generator/CnfGenerator.java
+++ src/generator/CnfGenerator.java
@@ -13,25 +13,41 @@ public class CnfGenerator {
         return generator_instance;
     }
 
-    private String generateCNF(int numClauses, int clauseMinLen, int clauseMaxLen, int maxProp) {
+    public static String generateCNF(int numClauses, int clauseMinLen, int clauseMaxLen, int maxProp) {
         String CNF = "";
 
         for (int i = 0; i < numClauses; i++) {
+            String clause = generateClause(clauseMinLen, clauseMaxLen, maxProp);
+            CNF = CNF.concat(clause);
 
+            if (i+1 < numClauses) {
+                CNF = CNF.concat("^");
+            }
         }
-
         return CNF;
     }
 
-    private String generateClause (int clauseMinLen, int clauseMaxLen, int maxProp) {
+    private static String generateClause (int clauseMinLen, int clauseMaxLen, int maxProp) {
         String clause = "(";
         Random randomGenerator = new Random();
         int clauseLen = randomGenerator.nextInt(clauseMaxLen) + clauseMinLen;
 
         for (int i = 0; i < clauseLen; i++) {
             boolean neg = randomGenerator.nextBoolean();
+            int prop = randomGenerator.nextInt(maxProp) + 1; //propositions named from 1 to maxProp
 
+            String literal = "";
+            if (neg) {
+                literal = literal.concat("-");
+            }
+            literal = literal.concat(String.valueOf(prop));
+            clause = clause.concat(literal);
+            if (i+1 < clauseLen) {
+                clause = clause.concat("v");
+            }
         }
+
+        clause = clause.concat(")");
 
         return clause;
     }
